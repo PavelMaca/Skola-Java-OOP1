77c06021079f93e48815fb4230a5997cf8f1cee5
Pavel Máca <maca.pavel@gmail.com>
2016-03-02 10:24:49 +0100

    init Zuul_Bad

---
+++ Cvičeni/Zuul_Bad/src/zuul_bad/Zuul_Bad.java
@@ -0,0 +1,19 @@
+package zuul_bad;
+
+/**
+ * Hlavni trida pro spusteni hry.
+ *
+ * @author Jaroslav Icha
+ */
+public class Zuul_Bad {
+
+    /**
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Game newGame = new Game();
+        newGame.play();
+    }
+
+}
